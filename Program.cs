e1e549e66dbd4913326cc9665c0d0577a4fc1714
FreeCactus <61978341+FreeCactus@users.noreply.github.com>
2020-03-10 19:45:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Переводы переменых в разные виды*/
+            ////int a = 258;
+            //byte b = (byte)a;
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+            //byte b = 208;
+            //    int a =(int)b;
+
+            //string o = "1246";
+            //int l = Convert.ToInt32(o);
+            //Console.WriteLine(l);
+
+            //int k = 30;
+            //string e = Convert.ToString(k);
+            //Console.WriteLine(e);
+
+            //int a = 2;
+            //string b = "lol " + a;
+            //Console.WriteLine(b);
+
+            //bool b = true;
+            //string a = Convert.ToString(b);
+            //Console.WriteLine(a);
+
+            //string L = "true";
+            //bool a = Convert.ToBoolean(L);
+            //Console.WriteLine(a);
+
+            //Если перевод в int возможен, то в результат
+            //if (int.TryParse("959823", out int result));
+            //Console.WriteLine(result);
+
+
+            
+
+
+            
+
+
+
+            
+
+
+        }
+    }
+}
+     
